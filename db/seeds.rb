7d6be05c529295a299514e75e9ff4e465b6d1f86
techno-tanoC <develop.tanoc@gmail.com>
2017-09-12 19:28:10 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
 require 'factory_girl'
 Dir[Rails.root.join('spec/factories/**/*.rb')].each { |f| require f }
 Rake::Task['db:migrate:reset'].invoke
+
+items = [
+  %w(Qiita http://qiita.com/),
+  %w(niconico http://www.nicovideo.jp/),
+  %w(github https://github.com/),
+  %w(Google https://www.google.co.jp/),
+]
+
+items.each do |title, url|
+  Item.create!(title: title, url: url)
+end
