a7e339a72f32688c2c438fbca1756b164c2851cf
techno-tanoC <develop.tanoc@gmail.com>
2017-09-16 10:11:47 +0900

    add tags for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,12 @@ Dir[Rails.root.join('spec/factories/**/*.rb')].each { |f| require f }
 Rake::Task['db:migrate:reset'].invoke
 
 items = [
-  %w(Qiita http://qiita.com/),
-  %w(niconico http://www.nicovideo.jp/),
-  %w(github https://github.com/),
-  %w(Google https://www.google.co.jp/),
+  ["Qiita", "http://qiita.com/", "programming"],
+  ["niconico", "http://www.nicovideo.jp/", "movie"],
+  ["github", "https://github.com/", "programming"],
+  ["Google", "https://www.google.co.jp/"],
 ]
 
-items.each do |title, url|
-  Item.create!(title: title, url: url)
+items.each do |title, url, tag|
+  Item.create!(title: title, url: url, tag_list: tag)
 end
