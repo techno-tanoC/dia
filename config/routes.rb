dd11c0a47a3cb7009c3b7baabdf00bdbc5a2e560
techno-tanoC <develop.tanoc@gmail.com>
2017-09-20 19:50:46 +0900

    tags page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
   get "/menu", to: "query#menu"
+  get "/tags", to: "query#tags"
   get "/search", to: "query#search"
   resources :items
   resources :rules
